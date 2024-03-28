257ce1d9b65ae0f91938d4b5361f88a43e4eb096
k0 <137244173+k0src@users.noreply.github.com>
2024-03-28 14:29:31 -0500

    Add files via upload

--- yt2mp3.py
+++ yt2mp3.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import ttkbootstrap as ttk
-from pytube import YouTube 
+from pytube import YouTube, Playlist
 import os
 from tkinter import filedialog
 from PIL import Image, ImageTk
@@ -12,29 +12,70 @@ photo = None
 def download():
     global photo
 
+    if 'youtube.com' not in str(download_string.get()) and 'youtu.be' not in str(download_string.get()):
+        error_message.set(f'{download_string.get()} is not a valid YouTube URL!')
+        return
+    
     directory_path = filedialog.askdirectory()
 
     if not directory_path:
         return
     
-    yt = YouTube(str(download_string.get()))
+    if ',' in str(download_string.get()):
+        urls = str(download_string.get()).split(',')
 
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path=directory_path)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+        for url in urls:
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=True).first()
+            out_file = video.download(output_path=directory_path)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+        
+        if batch_thumbs_option_var.get() == 1:
+            for url in urls:
+                yt = YouTube(url)
+                with urllib.request.urlopen(yt.thumbnail_url) as u:
+                    raw_data = u.read()
+                image = Image.open(io.BytesIO(raw_data))
+                image.save(f"{directory_path}/{yt.title}.jpg")
 
-    with urllib.request.urlopen(yt.thumbnail_url) as u:
-        raw_data = u.read()
-    image = Image.open(io.BytesIO(raw_data))
-    photo = ImageTk.PhotoImage(image)
+    elif 'list=' in str(download_string.get()):
+        yt = Playlist(str(download_string.get()))
 
-    video_label_var.set(yt.title)
+        for video in yt.videos:
+            video = video.streams.filter(only_audio=True).first()
+            out_file = video.download(output_path=directory_path)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
 
-    video_thumb.config(image=photo)
+        if batch_thumbs_option_var.get() == 1:
+            for video in yt.videos:
+                with urllib.request.urlopen(video.thumbnail_url) as u:
+                    raw_data = u.read()
+                image = Image.open(io.BytesIO(raw_data))
+                image.save(f"{directory_path}/{video.title}.jpg")
 
-    download_thumb_button['state'] = tk.NORMAL
+    else:
+        yt = YouTube(str(download_string.get()))
+
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=directory_path)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+        with urllib.request.urlopen(yt.thumbnail_url) as u:
+            raw_data = u.read()
+        image = Image.open(io.BytesIO(raw_data))
+        photo = ImageTk.PhotoImage(image)
+
+        video_label_var.set(f'{yt.title} - {yt.author}')
+
+        video_thumb.config(image=photo)
+
+        download_thumb_button['state'] = tk.NORMAL
 
 def download_thumb():
     directory_path = filedialog.askdirectory()
@@ -56,18 +97,26 @@ window.maxsize(1000, 780)
 window.minsize(1000, 780)
 
 video_label_var = tk.StringVar()
+error_message = tk.StringVar()
 
 title = ttk.Label(window, text="YouTube To Mp3", font = 'Calibri 24 bold')
 title.pack(pady=5)
 
-subtitle2 = ttk.Label(window, text="Paste a YouTube Link:", font = 'Calibri 18')
+subtitle2 = ttk.Label(window, text='Paste a YouTube Video or Playlist Link (or multiple videos seperated with ","):', font = 'Calibri 16')
 subtitle2.pack(pady=5)
 
 download_string = tk.StringVar()
 download_entry = ttk.Entry(window, textvariable=download_string, width=50)
 download_entry.pack(pady=5)
+error_label = ttk.Label(window, text='', textvariable=error_message, font='Calibri 14', foreground='red')
+error_label.pack(pady=5)
 download_button = ttk.Button(window, text='Download', command=download)
 download_button.pack(pady=5)
+
+batch_thumbs_option_var = tk.IntVar(value=0)
+
+batch_thumbs_option = ttk.Checkbutton(window, text='Batch Download Thumbnails', variable = batch_thumbs_option_var, onvalue=1, offvalue=0)
+batch_thumbs_option.pack(pady=5)
 
 
 video_thumb_label = ttk.Label(window, text="Video Thumbnail:", font='Calibri 18')
